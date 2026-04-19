8a22cf57373f49f451be22c6b8011f9cb098ce8e
Daniel <aybcaneyes@gmai.com>
2026-04-19 14:16:14 +0200

    cambios a PWA

---
+++ public/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'los-tainos-v1';
+const ASSETS = [
+  '/',
+  '/login.html',
+  '/favicon.png',
+  '/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch event (Network first, then fallback to cache)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
